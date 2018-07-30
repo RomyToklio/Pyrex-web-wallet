ff0ae6bd577609a02070a3dc73649fecc1b95ca8
RomyToklio <contact@evaveo.com>
2018-07-30 12:05:25 +0200

    update

--- html/js/config.js
+++ html/js/config.js
@@ -5,8 +5,8 @@ var config = {
     stagenetExplorerUrl: "http://pyrex-explorer.tokl.io:8083/",
     nettype: 0, /* 0 - MAINNET, 1 - TESTNET, 2 - STAGENET */
     coinUnitPlaces: 12,
-    txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-    txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
+    txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Pyrexcoin
+    txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Pyrexcoin
     coinSymbol: 'PYX',
     openAliasPrefix: "pyx",
     coinName: 'Pyrex',
